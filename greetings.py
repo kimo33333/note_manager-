fdba61df10146c366cea740946831f1f92593dbb
kimo33333 <karimhuss333@gmail.com>
2025-01-15 13:54:19 +0300

    Create greetings.py

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = "Karim"
+title = "Project for Chatbot Development"
+content = "This note describes the stages of chatbot development, requirements analysis, interface design, functionality implementation, and testing."
+status = "In Progress"
+created_date = "15-01-2025"
+issue_date = "20-02-2025"
+print("Karim", username)
+print("Project for Chatbot Development", title)
+print("This note describes the stages of chatbot development, requirements analysis, interface design, functionality implementation, and testing.", content )
+print("In Progress", status)
+print("15-01-2025", created_date)
+print("20-02-2025", issue_date)
