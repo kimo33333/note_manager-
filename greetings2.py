621ae0b8020653b249d48675207fb729e8aa6399
kimo33333 <karimhuss333@gmail.com>
2025-01-16 17:47:36 +0300

    Update greetings2.py

--- greetings2.py
+++ greetings2.py
@@ -2,3 +2,5 @@ name = input("Введите ваше имя: ")
 age = int(input("Введите ваш возраст: "))
 sum_result = age + 5
 print(f"Через 5 лет вам будет {sum_result}")
+created_date = input("Введите вашу дату рождения в формате 'ДД-ММ-ГГГГ' :")
+print(created_date[0:5])
