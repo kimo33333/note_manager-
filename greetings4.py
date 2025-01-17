f6703126807e2ad92a9f0f6cea1eb832232f22e1
kimo33333 <karimhuss333@gmail.com>
2025-01-17 14:47:33 +0300

    Create greetings4.py

---
+++ greetings4.py
@@ -0,0 +1,5 @@
+title1 = input("Введите первый заголовок: ")
+title2 = input("Введите второй заголовок: ")
+title3 = input("Введите третий заголовок: ")
+title = [title1, title2, title3]
+print(title)
