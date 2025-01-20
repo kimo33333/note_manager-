291f8e3ef6a3715bc566a3a73217a1b38aee78b7
kimo33333 <karimhuss333@gmail.com>
2025-01-20 14:10:03 +0300

    Update greetings.py

--- greetings.py
+++ greetings.py
@@ -4,9 +4,9 @@ content = "This note describes the stages of chatbot development, requirements a
 status = "In Progress"
 created_date = "15-01-2025"
 issue_date = "20-02-2025"
-print("Karim", username)
-print("Project for Chatbot Development", title)
-print("This note describes the stages of chatbot development, requirements analysis, interface design, functionality implementation, and testing.", content )
-print("In Progress", status)
-print("15-01-2025", created_date)
-print("20-02-2025", issue_date)
+print("Username:", username)
+print("Title:", title)
+print("Content:", content)
+print("Status:", status)
+print("Created Date:", created_date)
+print("Issue Date:", issue_date)
